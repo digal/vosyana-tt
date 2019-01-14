12e149506b408a9ba405dd66f0e579d1df8de2c9
Yuri Buyanov <yuri.buyanov@gmail.com>
2019-01-14 22:57:33 +0300

    flush logs in controller

--- Sources/App/Controllers/TTCallbackController.swift
+++ Sources/App/Controllers/TTCallbackController.swift
@@ -6,15 +6,19 @@
 //
 
 import Vapor
+import Foundation
 
 final class TTCallbackController {
     func handleMessage(_ req: Request) throws -> Future<String> {
+        
         print("TTCallbackController handleMessage: \(req)")
         if let data = req.http.body.data {
             print("body: \(String(data: data, encoding: .utf8) ?? "n/a")")
         } else {
             print("empty request")
         }
+        
+        fflush(stdout)
         
         return req.future("TT callback")
     }
